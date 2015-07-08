09afba32180d6e61c75c5aeba5b4bfd811fe8057
redaphid <a@octoblu.com>
2015-07-08 15:35:43 -0700

    travis, constructing the TERRIFYING TENTACLE for particle

---
+++ examples/tentacle-particle/tentacle-particle.ino
@@ -0,0 +1,35 @@
+#include <SPI.h>
+
+#include "tentacle-build.h"
+
+#define conn Serial
+
+TentacleArduino tentacle;
+Pseudopod pseudopod(conn, conn, tentacle);
+
+void setup() {
+  Serial.begin(57600);
+}
+
+void loop() {
+  readData();
+
+  if(!pseudopod.isConfigured()) {
+    delay(100);
+    pseudopod.requestConfiguration();
+  }
+
+  if(pseudopod.shouldBroadcastPins() ) {
+    delay(pseudopod.getBroadcastInterval());
+    pseudopod.sendConfiguredPins();
+  }
+}
+
+void readData() {
+  while (conn.available()) {
+    if(pseudopod.readMessage() == TentacleMessageTopic_action) {
+      pseudopod.sendPins();
+    }
+  }
+
+}
