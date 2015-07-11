eb729de98dac76e23fa108c3e252ef0fd2413f5f
redaphid <a@octoblu.com>
2015-07-11 15:23:45 -0700

    travis, constructing the TERRIFYING TENTACLE for particle

--- firmware/examples/tentacle-particle/tentacle-particle.ino
+++ firmware/examples/tentacle-particle/tentacle-particle.ino
@@ -1,33 +1,81 @@
-#include "tentacle-particle.h"
+// This #include statement was automatically added by the Spark IDE.
+#include "tentacle-particle/tentacle-particle.h"
 
-#define conn Serial
+// #define server "tentacle.octoblu.com"
+// #define port 80
+
+
+IPAddress server(192,168,0,112);
+#define port 8111
+
+static const char uuid[]  = "ff12c403-04c7-4e63-9073-2e3b1f8e4450";
+static const char token[] = "28d2c24dfa0a5289799a345e683d570880a3bc41";
+
+TCPClient conn;
 
 TentacleArduino tentacle;
 Pseudopod pseudopod(conn, conn, tentacle);
 
 void setup() {
-  Serial.begin(57600);
+  Serial.begin(9600);
+  Serial.println(F("The Day of the Tentacle has begun!"));
+  waitForWifi();
+  connectToServer();
 }
 
 void loop() {
-  readData();
-
-  if(!pseudopod.isConfigured()) {
-    delay(100);
-    pseudopod.requestConfiguration();
+  if(!WiFi.ready()) {
+      Serial.println(F("WiFi wasn't ready. waiting."));
   }
+  if (!conn.connected()) {
+    conn.stop();
+    connectToServer();
+  }
+
+  readData();
 
   if(pseudopod.shouldBroadcastPins() ) {
     delay(pseudopod.getBroadcastInterval());
-    pseudopod.sendConfiguredPins();
+    size_t configSize = pseudopod.sendConfiguredPins();
+    Serial.print(configSize);
+    Serial.print(F(" bytes written while broadcasting pins"));
   }
 }
 
 void readData() {
+
   while (conn.available()) {
+    Serial.println(F("Received message"));
+    Serial.flush();
+
     if(pseudopod.readMessage() == TentacleMessageTopic_action) {
       pseudopod.sendPins();
     }
   }
+
+}
+
+void waitForWifi() {
+    while(!WiFi.ready()) {
+        Serial.println(F("Waiting for wifi"));
+        delay(200);
+    }
+}
+
+void connectToServer() {
+  int connectionAttempts = 0;
+  Serial.println(F("Connecting to the server."));
+  Serial.flush();
+
+  while(!conn.connect(server, port)) {
+    Serial.println(F("Can't connect to the server."));
+    Serial.flush();
+    conn.stop();
+    connectionAttempts++;
+  }
+
+  size_t authSize = pseudopod.authenticate(uuid, token);
+  Serial.print(authSize);
+  Serial.println(F(" bytes written for authentication"));
 
 }
